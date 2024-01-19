5d89890341ca5d20b280382f239d7b7a4accfc31
Gabriel Sprecher Dutra <gabrielsprecherdutra@gmail.com>
2024-01-18 21:36:32 -0300

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,94 @@
+'use client'
+import { Redirect } from 'next';
 import Image from 'next/image'
+import { useState } from 'react'
+import { Roboto_Slab } from 'next/font/google';
+
+  const roboto = Roboto_Slab({
+    weight: '400',
+    subsets: ['latin'],
+  })
 
 export default function Home() {
+
+  interface Rendimentos {
+    month: number;
+    moneyInvested: number;
+    moneyFromInterest: number;
+    moneyTotal: number;
+  }
+
+  const [startValue,setStartValue] = useState<number>(0)
+  const [monthlyValue,setMonthlyValue] = useState<number>(0)
+  const [interestRate,setInterestRate] = useState<number>(0)
+
+  const calculateIncome = () => {
+    var YEARS: number = 30;
+    var amount: number = startValue;
+    var returnArray: Array<Rendimentos> = [];
+
+    for (let i = 1; i<=YEARS*12; i++) {
+      amount = Number(amount) + Number(monthlyValue);
+      amount = Number(amount) * (1+Number(interestRate/100));
+      returnArray.push({
+        month: i,
+        moneyInvested: i*monthlyValue + Number(startValue),
+        moneyFromInterest: amount - (i*monthlyValue + Number(startValue)),
+        moneyTotal: amount
+      })
+    }
+
+    return returnArray;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="min-h-screen padding-main justify-around">
+      <div className="basic-container title">
+        <h1 className={roboto.className}>Calculadora de investimentos e juros compostos</h1>
+      </div>
+      <div className="input-container basic-container">
+        <div className="input">
+            <a>Quantia Inicial:</a>
+            <input
+              type="number"
+              id="startValue"
+              value={startValue}
+              onChange={(e:any) => setStartValue(e.target.value)}
             />
-          </a>
+            <a>Quantia mensal:</a>
+            <input
+              type="number"
+              id="startValue"
+              value={monthlyValue}
+              onChange={(e:any) => setMonthlyValue(e.target.value)}
+            />
+            <a>Rendimento mensal em %:</a>
+            <input
+              type="number"
+              id="startValue"
+              value={interestRate}
+              onChange={(e:any) => setInterestRate(e.target.value)}
+            />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='results basic-container'>
+        <a>Rendimentos:</a>
+        <div className="border">
+          <div className='results-div'>
+              <p Style="width: 10%;">Mês:</p>
+              <p Style="width: 30%;">Valor total investido:</p>
+              <p Style="width: 30%;">Rendimentos:</p>
+              <p Style="width: 30%;">Valor total:</p>
+            </div>
+          {calculateIncome().map((e:Rendimentos, i: number)=>(
+            <div className='results-div' key={i}>
+              <p Style="width: 10%;"> {e.month} </p>
+              <p Style="width: 30%;"> {e.moneyInvested.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} </p>
+              <p Style="width: 30%;"> {e.moneyFromInterest.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} </p>
+              <p Style="width: 30%;"> {e.moneyTotal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} </p>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   )
